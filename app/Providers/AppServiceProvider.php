3f7e44b63f6ac0bbdff753182e5ed63b5600c20e
Ly Minh Thien Tuong <tuong23092006205@vnkgu.edu.vn>
2026-06-22 22:39:11 +0700

    Initial commit - Website ban dien thoai may tinh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Đăng ký các dịch vụ của ứng dụng.
      */
     public function register(): void
     {
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap any application services.
+     * Nạp/Chạy chương trình mồi cho các dịch vụ ứng dụng
      */
     public function boot(): void
     {
